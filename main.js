84e809f3733b49c17674057defa8579f040a769d
Cristián Rojas <injcristianrojas@gmail.com>
2024-10-16 19:40:11 -0300

    Coment removal

--- main.js
+++ main.js
@@ -129,9 +129,6 @@ const Users = {
                         this.rows = result
                         console.log(result)
                     },
-                    // Note: it's important to handle errors here
-                    // instead of a catch() block so that we don't swallow
-                    // exceptions from actual bugs in components.
                     (error) => {
                         console.log(error)
                     }
